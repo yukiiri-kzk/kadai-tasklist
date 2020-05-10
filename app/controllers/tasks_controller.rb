4fbcb3dd39fbcbf2d21415f7a07c2cd1b448e021
kazuki.yukiiri <yukiiri.kazuki@gmail.com>
2020-05-10 00:01:26 +0000

    add db-seed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index 
-    @tasks = Task.all.page(params[:page]).per(15)
+    @tasks = Task.all.page(params[:page]).per(25)
   end
     
   def show
