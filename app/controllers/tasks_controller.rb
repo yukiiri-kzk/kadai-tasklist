8765102d999702d6a314712a61cebc4eb9a1aa26
kazuki.yukiiri <yukiiri.kazuki@gmail.com>
2020-05-14 12:47:51 +0000

    Fix: route and tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,10 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
   def task_params
